8da7a4bc5ad304c4fb9895660e0876afffb37d4f
WYSIWYP <stuartbyrom@gmail.com>
2020-04-16 14:00:11 +0200

    cumulative change

---
+++ precache-manifest.3589723f375d40128016e752596ee649.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63c866bd5343318b257d4d5ef44ac9b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88d5ee4d37be182b679b",
+    "url": "/static/css/main.422f8397.chunk.css"
+  },
+  {
+    "revision": "b4e1a15ed7842cd8d7b1",
+    "url": "/static/js/2.db225748.chunk.js"
+  },
+  {
+    "revision": "e4bfa755afd2c20460d89b69d475f2cd",
+    "url": "/static/js/2.db225748.chunk.js.LICENSE"
+  },
+  {
+    "revision": "88d5ee4d37be182b679b",
+    "url": "/static/js/main.77cd0d77.chunk.js"
+  },
+  {
+    "revision": "a4084bd36f2abba05ee9",
+    "url": "/static/js/runtime~main.7ceaa85f.js"
+  },
+  {
+    "revision": "2ec809837ce813c94da7b93fbaa4dbc3",
+    "url": "/static/media/dropDown.2ec80983.svg"
+  },
+  {
+    "revision": "de0869e324680c99efa1250515b4b41c",
+    "url": "/static/media/open-sans-v17-latin-regular.de0869e3.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6eb80cd7433dcade9747239d6b8912de.js"
+  "/precache-manifest.3589723f375d40128016e752596ee649.js"
 );
 
 self.addEventListener('message', (event) => {
